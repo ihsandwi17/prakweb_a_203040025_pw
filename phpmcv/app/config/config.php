8de864c57755d99e1435335d13c624af826001d6
ihsandwi17 <nurihsan719@gmail.com>
2022-10-10 20:27:59 +0700

    menyelesaikan video 8 Membuat Aplikasi MVC dengan PHP #8 Database Wrapper

---
+++ phpmcv/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+define('BASEURL', 'http://localhost/prakweb_a_203040025_pw/pw/phpmcv/public');
+
+// db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmcv');
